6c2bbcd00372c1d31986a5d1ae75d52d9d58e233
Shichisan <taiga.shichisan@gmail.com>
2023-01-08 14:09:18 +0900

    implementation authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './jwt-auth/jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth/local-auth.guard';
+import type { Request as ExpressRequest } from 'express';
+import type { User } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: ExpressRequest & { user: User }) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: ExpressRequest & { user: User }) {
+    return req.user;
   }
 }
